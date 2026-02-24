12a7f976bff9e8a93aa5712fdfe7351bddd3bcb2
Cássio dos Santos Sousa <cassiosantossousa@gmail.com>
2026-02-24 04:19:25 -0300

    refactor: no need to repeat all test scenarios like that

--- src/components/stubs/StatusBadge.test.ts
+++ src/components/stubs/StatusBadge.test.ts
@@ -3,63 +3,21 @@ import { render, screen } from '@testing-library/svelte'
 import StatusBadge from './StatusBadge.svelte'
 
 describe('<StatusBadge />', () => {
-  it('renders a status badge for Open status', () => {
-    render(StatusBadge, { props: { status: 'Open' } })
-    const div = screen.getByTitle('Open')
-    expect(div).toBeTruthy()
-  })
-
-  it('renders a status badge for InProgress status', () => {
-    render(StatusBadge, { props: { status: 'InProgress' } })
-    const div = screen.getByTitle('In Progress')
-    expect(div).toBeTruthy()
-  })
-
-  it('renders a status badge for InReview status', () => {
-    render(StatusBadge, { props: { status: 'InReview' } })
-    const div = screen.getByTitle('In Review')
-    expect(div).toBeTruthy()
-  })
-
-  it('renders a status badge for Completed status', () => {
-    render(StatusBadge, { props: { status: 'Completed' } })
-    const div = screen.getByTitle('Completed')
-    expect(div).toBeTruthy()
-  })
-
-  it('renders a status badge for Canceled status', () => {
-    render(StatusBadge, { props: { status: 'Canceled' } })
-    const div = screen.getByTitle('Canceled')
-    expect(div).toBeTruthy()
-  })
-
-  it('displays the correct icon for Open status', () => {
-    render(StatusBadge, { props: { status: 'Open' } })
-    const icon = screen.getByText('circle')
-    expect(icon).toBeTruthy()
-  })
-
-  it('displays the correct icon for InProgress status', () => {
-    render(StatusBadge, { props: { status: 'InProgress' } })
-    const icon = screen.getByText('clock_loader_40')
-    expect(icon).toBeTruthy()
-  })
-
-  it('displays the correct icon for Completed status', () => {
-    render(StatusBadge, { props: { status: 'Completed' } })
-    const icon = screen.getByText('check_circle')
-    expect(icon).toBeTruthy()
-  })
-
-  it('displays the correct icon for Canceled status', () => {
-    render(StatusBadge, { props: { status: 'Canceled' } })
-    const icon = screen.getByText('cancel')
-    expect(icon).toBeTruthy()
-  })
-
-  it('displays the correct icon for InReview status', () => {
-    render(StatusBadge, { props: { status: 'InReview' } })
-    const icon = screen.getByText('pending')
-    expect(icon).toBeTruthy()
-  })
+  it.each`
+    status          | title            | icon
+    ${'Open'}       | ${'Open'}        | ${'circle'}
+    ${'InProgress'} | ${'In Progress'} | ${'clock_loader_40'}
+    ${'InReview'}   | ${'In Review'}   | ${'pending'}
+    ${'Completed'}  | ${'Completed'}   | ${'check_circle'}
+    ${'Canceled'}   | ${'Canceled'}    | ${'cancel'}
+  `(
+    'renders status badge for $status status with title $title and icon $icon',
+    ({ status, title, icon }) => {
+      render(StatusBadge, { props: { status } })
+      const div = screen.getByTitle(title)
+      expect(div).toBeTruthy()
+      const iconElement = screen.getByText(icon)
+      expect(iconElement).toBeTruthy()
+    }
+  )
 })
